02b39cffe1db1d53a0d265d3d28e7d409f28bf84
Gustavo Andrade <gustavoandrade82@gmail.com>
2016-02-16 21:43:15 -0200

    Having fun with Spring Cloud stack

--- reservation-client/src/main/java/org/test/ReservationClientApplication.java
+++ reservation-client/src/main/java/org/test/ReservationClientApplication.java
@@ -1,19 +1,42 @@
 package org.test;
 
+import com.netflix.governator.annotations.binding.Request;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.io.Resource;
+import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import javax.ws.rs.POST;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
+@EnableBinding(Source.class)
 @EnableZuulProxy
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableCircuitBreaker
 public class ReservationClientApplication {
 
 	public static void main(String[] args) {
@@ -22,14 +45,58 @@ public class ReservationClientApplication {
 }
 
 @RestController
+@RequestMapping("/reservations")
 class ReservationApiGatewayRestController {
 
 	@Autowired @LoadBalanced
 	RestTemplate restTemplate;
 
-	public Collection<String> getReservationName() {
-		// this.restTemplate.exchange("http://reservation-client")
-		return null;
+	@Output(Source.OUTPUT)
+	@Autowired
+	private MessageChannel messageChannel;
+
+	public Collection<String> getReservationNamesFallback(){
+		return Collections.emptyList();
 	}
 
+	@HystrixCommand(fallbackMethod = "getReservationNamesFallback")
+	@RequestMapping("/names")
+	public Collection<String> getReservationNames() {
+		ParameterizedTypeReference<Resources<Reservation>> ptr = new ParameterizedTypeReference<Resources<Reservation>>() {};
+		ResponseEntity<Resources<Reservation>> responseEntity = this.restTemplate.exchange("http://reservation-service/reservations", HttpMethod.GET, null, ptr);
+
+		return responseEntity
+				.getBody()
+				.getContent()
+				.stream()
+				.map(Reservation::getReservationName)
+				.collect(Collectors.toList());
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public void write(@RequestBody Reservation r){
+		this.messageChannel.send(MessageBuilder.withPayload(r.getReservationName()).build());
+	}
+
+}
+
+class Reservation {
+	private Long id;
+	private String reservationName;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getReservationName() {
+		return reservationName;
+	}
+
+	public void setReservationName(String reservationName) {
+		this.reservationName = reservationName;
+	}
 }
--- reservation-service/src/main/java/org/test/ReservationServiceApplication.java
+++ reservation-service/src/main/java/org/test/ReservationServiceApplication.java
@@ -1,16 +1,21 @@
 package org.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +26,7 @@ import javax.persistence.Id;
 import java.util.Arrays;
 import java.util.Collection;
 
+@EnableBinding(Sink.class)
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ReservationServiceApplication {
@@ -38,6 +44,20 @@ public class ReservationServiceApplication {
 		SpringApplication.run(ReservationServiceApplication.class, args);
 	}
 }
+
+
+@MessageEndpoint
+class MessageReservationReceiver {
+
+	@Autowired
+	ReservationRepository reservationRepository;
+
+	@ServiceActivator(inputChannel = Sink.INPUT)
+	public void acceptReservation(String rn) {
+		reservationRepository.save(new Reservation(rn));
+	}
+}
+
 
 @RefreshScope
 @RestController
